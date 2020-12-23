f8c9555215b2e99c7bd08853951d56e8a0eaf564
Manpreet singh <ms4110415@gmail.com>
2020-12-23 20:06:20 +0530

    Updates

---
+++ precache-manifest.b8a902cc8ce35c65b9aa3c6d00b72ff3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32182662ea3afd8cb8ee28f7d06297dd",
+    "url": "/MillionColors/index.html"
+  },
+  {
+    "revision": "6f26afb8d0025514c01f",
+    "url": "/MillionColors/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7c215fc07854dc194e5a",
+    "url": "/MillionColors/static/css/main.017d0bb6.chunk.css"
+  },
+  {
+    "revision": "6f26afb8d0025514c01f",
+    "url": "/MillionColors/static/js/2.8d863e8c.chunk.js"
+  },
+  {
+    "revision": "a5ce290b3880d4708ed8e6c83b711663",
+    "url": "/MillionColors/static/js/2.8d863e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c215fc07854dc194e5a",
+    "url": "/MillionColors/static/js/main.37de820b.chunk.js"
+  },
+  {
+    "revision": "fc77eaa80bd211ad2b73",
+    "url": "/MillionColors/static/js/runtime-main.1faf3e30.js"
+  },
+  {
+    "revision": "0c586d0656f89accba734d432c026d6d",
+    "url": "/MillionColors/static/media/Logo.0c586d06.png"
+  },
+  {
+    "revision": "8280dabac66a11e34ee8bacb7b208fcb",
+    "url": "/MillionColors/static/media/newimg.8280daba.png"
+  }
+]);
